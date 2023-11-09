71a5e5f40db04e31c9700a4fae9315f317dd69cf
IraITaran <105692178+IraITaran@users.noreply.github.com>
2023-11-09 21:37:31 +0100

    Fix build linter errors for deployments
    Fix build linter errors for deployments

--- src/Components/KnowledgeBase/KnowledgeBase.js
+++ src/Components/KnowledgeBase/KnowledgeBase.js
@@ -13,134 +13,122 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 const KnowledgeBase = () => {
-    const [slidesPerView, setSliderPerView] = useState("1");
+  const [slidesPerView] = useState("1");
 
-    const changeSlidesPerView = (event) => {
-        console.log(event.target);
-        //setSliderPerView("2");
-    };
+  const changeSlidesPerView = (event) => {
+    console.log(event.target);
+    //setSliderPerView("2");
+  };
 
-    return (
-        <div className="KnowledgeBase" onResize={changeSlidesPerView}>
-            <div className="container">
-                <Swiper
-                    modules={[Navigation, Autoplay]}
-                    spaceBetween={50}
-                    slidesPerView={slidesPerView}
-                    navigation
-                    scrollbar={{ draggable: true }}
-                    loop={true}
-                    autoplay={{
-                        delay: 5000,
-                        disableOnInteraction: false,
-                    }}
-                    breakpoints={{
-                        640: {
-                            slidesPerView: 1,
-                        },
-                        768: {
-                            slidesPerView: 2,
-                        },
-                        920: {
-                            slidesPerView: 3,
-                        },
-                    }}
-                >
-                    <h2 className="knowledgebase-title">База знаний</h2>
-                    <SwiperSlide>
-                        <a className="slide-link" href="#">
-                            <img
-                                src={SlideImage1}
-                                alt="slide-1"
-                                className="slide-image"
-                            ></img>
-                            <h3 className="slide-title">
-                                Новый спотовый листинг GCAKE/USDT
-                            </h3>
-                            <p className="slide-text">
-                                Купите и получите часть призового фонда в 40 000
-                            </p>
-                        </a>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <a href="#" className="slide-link">
-                            <img
-                                src={SlideImage2}
-                                alt="slide-2"
-                                className="slide-image"
-                            ></img>
-                            <h3 className="slide-title">
-                                Новый спотовый листинг GCAKE/USDT
-                            </h3>
-                            <p className="slide-text">
-                                Купите и получите часть призового фонда в 40 000
-                            </p>
-                        </a>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <a href="#" className="slide-link">
-                            <img
-                                src={SlideImage3}
-                                alt="slide-3"
-                                className="slide-image"
-                            ></img>
-                            <h3 className="slide-title">
-                                Новый спотовый листинг GCAKE/USDT
-                            </h3>
-                            <p className="slide-text">
-                                Купите и получите часть призового фонда в 40 000
-                            </p>
-                        </a>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <a href="#" className="slide-link">
-                            <img
-                                src={SlideImage3}
-                                alt="slide-3"
-                                className="slide-image"
-                            ></img>
-                            <h3 className="slide-title">
-                                Новый спотовый листинг GCAKE/USDT
-                            </h3>
-                            <p className="slide-text">
-                                Купите и получите часть призового фонда в 40 000
-                            </p>
-                        </a>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <a href="#" className="slide-link">
-                            <img
-                                src={SlideImage3}
-                                alt="slide-3"
-                                className="slide-image"
-                            ></img>
-                            <h3 className="slide-title">
-                                Новый спотовый листинг GCAKE/USDT
-                            </h3>
-                            <p className="slide-text">
-                                Купите и получите часть призового фонда в 40 000
-                            </p>
-                        </a>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <a href="#" className="slide-link">
-                            <img
-                                src={SlideImage3}
-                                alt="slide-3"
-                                className="slide-image"
-                            ></img>
-                            <h3 className="slide-title">
-                                Новый спотовый листинг GCAKE/USDT
-                            </h3>
-                            <p className="slide-text">
-                                Купите и получите часть призового фонда в 40 000
-                            </p>
-                        </a>
-                    </SwiperSlide>
-                </Swiper>
-            </div>
-        </div>
-    );
+  return (
+    <div className="KnowledgeBase" onResize={changeSlidesPerView}>
+      <div className="container">
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          spaceBetween={50}
+          slidesPerView={slidesPerView}
+          navigation
+          scrollbar={{ draggable: true }}
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            920: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          <h2 className="knowledgebase-title">База знаний</h2>
+          <SwiperSlide>
+            <a className="slide-link" href="/">
+              <img
+                src={SlideImage1}
+                alt="slide-1"
+                className="slide-image"
+              ></img>
+              <h3 className="slide-title">Новый спотовый листинг GCAKE/USDT</h3>
+              <p className="slide-text">
+                Купите и получите часть призового фонда в 40 000
+              </p>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/" className="slide-link">
+              <img
+                src={SlideImage2}
+                alt="slide-2"
+                className="slide-image"
+              ></img>
+              <h3 className="slide-title">Новый спотовый листинг GCAKE/USDT</h3>
+              <p className="slide-text">
+                Купите и получите часть призового фонда в 40 000
+              </p>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/" className="slide-link">
+              <img
+                src={SlideImage3}
+                alt="slide-3"
+                className="slide-image"
+              ></img>
+              <h3 className="slide-title">Новый спотовый листинг GCAKE/USDT</h3>
+              <p className="slide-text">
+                Купите и получите часть призового фонда в 40 000
+              </p>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/" className="slide-link">
+              <img
+                src={SlideImage3}
+                alt="slide-3"
+                className="slide-image"
+              ></img>
+              <h3 className="slide-title">Новый спотовый листинг GCAKE/USDT</h3>
+              <p className="slide-text">
+                Купите и получите часть призового фонда в 40 000
+              </p>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/" className="slide-link">
+              <img
+                src={SlideImage3}
+                alt="slide-3"
+                className="slide-image"
+              ></img>
+              <h3 className="slide-title">Новый спотовый листинг GCAKE/USDT</h3>
+              <p className="slide-text">
+                Купите и получите часть призового фонда в 40 000
+              </p>
+            </a>
+          </SwiperSlide>
+          <SwiperSlide>
+            <a href="/" className="slide-link">
+              <img
+                src={SlideImage3}
+                alt="slide-3"
+                className="slide-image"
+              ></img>
+              <h3 className="slide-title">Новый спотовый листинг GCAKE/USDT</h3>
+              <p className="slide-text">
+                Купите и получите часть призового фонда в 40 000
+              </p>
+            </a>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default KnowledgeBase;
--- src/Components/ListOrTableDisplay/ListOrTableDisplay.js
+++ src/Components/ListOrTableDisplay/ListOrTableDisplay.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ListOrTableDisplay.css";
 import ListIcon from "../../images/list-icon.svg";
@@ -7,40 +7,36 @@ import TableIcon from "../../images/table-icon.svg";
 import TableIconUnactive from "../../images/table-icon-unactive.svg";
 
 export default function ListOrTableDisplay(props) {
-   
-    function displayList() {
-        props.setIsList(true);
-    }
+  function displayList() {
+    props.setIsList(true);
+  }
 
-    function displayTable() {
-        props.setIsList(false);
-    }
+  function displayTable() {
+    props.setIsList(false);
+  }
 
-    return (
-        <div className="ListOrTableDisplay d-flex align-items-center ">
-            <div
-                className="me-2 ms-auto"
-                onClick={() => {
-                    displayTable();
-                    props.isView();
-                }}
-            >
-                <img
-                    src={!props.isList ? TableIcon : TableIconUnactive}
-                    alt="table"
-                ></img>
-            </div>
-            <div
-                onClick={() => {
-                    displayList();
-                    props.isView();
-                }}
-            >
-                <img
-                    src={props.isList ? ListIcon : ListIconUnActive}
-                    alt="list"
-                ></img>
-            </div>
-        </div>
-    );
+  return (
+    <div className="ListOrTableDisplay d-flex align-items-center ">
+      <div
+        className="me-2 ms-auto"
+        onClick={() => {
+          displayTable();
+          props.isView();
+        }}
+      >
+        <img
+          src={!props.isList ? TableIcon : TableIconUnactive}
+          alt="table"
+        ></img>
+      </div>
+      <div
+        onClick={() => {
+          displayList();
+          props.isView();
+        }}
+      >
+        <img src={props.isList ? ListIcon : ListIconUnActive} alt="list"></img>
+      </div>
+    </div>
+  );
 }
